4852123637c4635d9bda34e46dad09da9cfa8634
fenghaojiang <457420285@qq.com>
2021-12-05 14:50:46 +0800

    add client

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "flag"
+
+var (
+	httpAddr    = flag.String("svc", "localhost:51000", "service host:port for this node")
+	raftId      = flag.String("id", "", "node id used by Raft")
+	raftDataDir = flag.String("data", "data/", "raft data dir")
+	raftAddr    = flag.String("raft", "localhost:52000", "raft host:port for this node")
+	joinAddr    = flag.String("join", "", "join address")
+	clientAddr  = flag.String("service_join", "localhost:50000", "raft client port")
+)
+
 func main() {
 
 }
