16cd2b78b868fe5c8fc8bee259156fa63cc3e17b
fenghaojiang <457420285@qq.com>
2021-12-05 15:22:02 +0800

    add notice

--- proto.gen.go
+++ proto.gen.go
@@ -1,3 +1,5 @@
 package main
 
+// notice: check your gogo protobuf version path
+
 //go:generate protoc -I=.\proto -I=$GOPATH\pkg -I=$GOPATH\pkg\mod\github.com\gogo\protobuf@v1.3.2 --gogofaster_out=plugins=grpc:.\proto .\proto\rpc_service.proto
